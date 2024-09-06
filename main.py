9fc8e1d49e8fccc9897ee9877cde819b7a4b8082
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-09-06 23:33:55 +0300

    Добавлено логирование

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
+from loguru import logger
 
 # URL начальной страницы
 base_url = 'https://belbazar24.by'
 start_url = 'https://belbazar24.by/women/dress/?filtr=3:33;3:34;3:35;3:36;3:37;3:38;3:39;3:40;3:41;3:42;3:43;3:44;3:45;3:46;3:47;3:48;3:49;3:50;3:51;3:52;3:53;3:54;3:55;3:56;3:57;3:58;3:59;3:60;3:61;6:73;6:74;6:75;6:76;6:77;6:78;6:79;6:80;6:81;6:82;6:83;6:84;6:85;6:86;6:87;6:88'
 
+logger.add("log/log.log", rotation="1 MB", compression="zip")  # Логирование программы
 
 def get_html(url):
     """Получить HTML-код страницы по URL."""
@@ -12,7 +14,7 @@ def get_html(url):
     if response.status_code == 200:
         return response.text
     else:
-        print(f"Не удалось получить страницу {url}. Код состояния: {response.status_code}")
+        logger.info(f"Не удалось получить страницу {url}. Код состояния: {response.status_code}")
         return None
 
 
@@ -43,12 +45,14 @@ def scrape_all_pages(start_url):
     all_product_links = []
 
     while current_url:
-        print(f"Обрабатывается страница: {current_url}")
+        logger.info(f"Обрабатывается страница: {current_url}")
         html_content = get_html(current_url)
         if html_content:
             soup = BeautifulSoup(html_content, 'html.parser')
             # Извлечение ссылок на товары
             product_links = extract_product_links(soup)
+            logger.info(f"Ссылки на товары: {product_links}")
+            logger.info(f"Найдено {len(product_links)} товаров")
             all_product_links.extend(product_links)
 
             # Поиск следующей страницы
@@ -63,6 +67,6 @@ def scrape_all_pages(start_url):
 all_product_links = scrape_all_pages(start_url)
 
 # Вывод всех найденных ссылок на товары
-print("\nВсе ссылки на товары:")
+logger.info("\nВсе ссылки на товары:")
 for link in all_product_links:
     print(link)
