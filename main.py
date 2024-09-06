848a469c0ff9e8227b2596c373e821f04d35de02
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-09-07 00:09:50 +0300

    Анализирую пагинацию, получение наибольшего значения и сбор всех ссылок для дальнейшего парсинга.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 from loguru import logger
@@ -7,6 +9,7 @@ base_url = 'https://belbazar24.by'
 start_url = 'https://belbazar24.by/women/dress/?filtr=3:33;3:34;3:35;3:36;3:37;3:38;3:39;3:40;3:41;3:42;3:43;3:44;3:45;3:46;3:47;3:48;3:49;3:50;3:51;3:52;3:53;3:54;3:55;3:56;3:57;3:58;3:59;3:60;3:61;6:73;6:74;6:75;6:76;6:77;6:78;6:79;6:80;6:81;6:82;6:83;6:84;6:85;6:86;6:87;6:88'
 
 logger.add("log/log.log", rotation="1 MB", compression="zip")  # Логирование программы
+
 
 def get_html(url):
     """Получить HTML-код страницы по URL."""
@@ -31,20 +34,41 @@ def extract_product_links(soup):
     return product_links
 
 
-def find_next_page(soup):
-    """Найти ссылку на следующую страницу."""
-    next_page_link = soup.find('a', class_='navigation_link_right')
-    if next_page_link and next_page_link.get('href'):
-        return base_url + next_page_link.get('href')
-    return None
+def find_max_page(soup):
+    """Найти наибольший номер страницы из навигации."""
+    max_page = 1
+    # Поиск всех ссылок на страницы в элементе с классом 'navigation_content'
+    page_numbers = soup.select('.navigation_content a')
+    for page_number in page_numbers:
+        try:
+            # Попытка получить номер страницы как целое число
+            number = int(page_number.text.strip())
+            if number > max_page:
+                max_page = number
+        except ValueError:
+            continue
+    return max_page
 
 
 def scrape_all_pages(start_url):
     """Собирать ссылки на товары со всех страниц с пагинацией."""
-    current_url = start_url
+    # Получаем HTML начальной страницы
+    html_content = get_html(start_url)
+    if not html_content:
+        return []
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    # Определяем максимальное количество страниц
+    max_page = find_max_page(soup)
+    logger.info(f"Наибольшая страница: {max_page}")
+
     all_product_links = []
 
-    while current_url:
+    # Цикл по всем страницам
+    for page in range(1, max_page + 1):
+        current_url = f"{start_url}&page={page}"
+        time.sleep(0.2)
         logger.info(f"Обрабатывается страница: {current_url}")
         html_content = get_html(current_url)
         if html_content:
@@ -54,11 +78,8 @@ def scrape_all_pages(start_url):
             logger.info(f"Ссылки на товары: {product_links}")
             logger.info(f"Найдено {len(product_links)} товаров")
             all_product_links.extend(product_links)
-
-            # Поиск следующей страницы
-            current_url = find_next_page(soup)
         else:
-            break
+            logger.info(f"Ошибка при получении страницы {current_url}")
 
     return all_product_links
 
@@ -69,4 +90,4 @@ all_product_links = scrape_all_pages(start_url)
 # Вывод всех найденных ссылок на товары
 logger.info("\nВсе ссылки на товары:")
 for link in all_product_links:
-    print(link)
+    logger.info(link)
