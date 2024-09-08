c41e3cef2be16d07e2db62fa41d79d9b5edb7a20
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-09-08 22:02:57 +0300

    Создание меню на flask

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from database import add_user, remove_duplicates
 
 # URL начальной страницы
 base_url = 'https://belbazar24.by'
-start_url = 'https://belbazar24.by/women/dress/?filtr=3:33;3:34;3:35;3:36;3:37;3:38;3:39;3:40;3:41;3:42;3:43;3:44;3:45;3:46;3:47;3:48;3:49;3:50;3:51;3:52;3:53;3:54;3:55;3:56;3:57;3:58;3:59;3:60;3:61;6:73;6:74;6:75;6:76;6:77;6:78;6:79;6:80;6:81;6:82;6:83;6:84;6:85;6:86;6:87;6:88'
 
 logger.add("log/log.log", rotation="1 MB", compression="zip")  # Логирование программы
 
@@ -75,7 +74,3 @@ def scrape_all_pages(start_url):
                 logger.info(f"Ошибка при получении страницы {current_url}")
     except Exception as e:
         logger.exception(e)
-
-
-if __name__ == '__main__':
-    scrape_all_pages(start_url)# Запуск сбора данных
